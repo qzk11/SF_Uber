893479d76188591c9e9fff23eb410c204f548432
qzk11 <616341668@qq.com>
2020-04-21 00:05:46 -0400

    render distance

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-var bodyParser = require('body-parser');
-var express = require("express");
-var app = express();
-
+const bodyParser = require('body-parser');
+const express = require("express");
+const app = express();
+const { spawn } = require('child_process');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-   extended: false
+    extended: false
 }));
 
 app.use(express.static('public'));
-app.get("/", function(req,res) {
+app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
-app.get("/visualization", function(req,res) {
+app.get("/visualization", function (req, res) {
     res.sendFile(__dirname + "/public/src/index.html");
 });
-app.get("/contact", function(req,res) {
+app.get("/contact", function (req, res) {
     res.sendFile(__dirname + "/public/src/contact.html");
 });
-app.listen(process.env.PORT, process.env.IP, function() {
+app.listen(process.env.PORT, process.env.IP, function () {
     console.log("Server has started");
 });
 
@@ -27,4 +27,12 @@ app.post("/prediction", function (req, res) {
     // res.send(pos1);
     console.log(pos1, pos2);
 
+    // spawn new child process to call the python script
+    const pythonProcess = spawn('python', [__dirname +'/public/src/distance.py', pos1, pos2]);
+    pythonProcess.stdout.on('data', function (data) {
+        console.log('Pipe data from python script ...');
+        dataToSend = data.toString();
+        console.log(dataToSend);
+    });
+    res.send("hello");
 })
